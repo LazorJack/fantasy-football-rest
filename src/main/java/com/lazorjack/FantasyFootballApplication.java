c2594bc83f7ec7759da8545471f8746bfded2213
Jack <jacklazorchak@yahoo.com>
2016-10-29 15:03:52 -0400

    Initial commit from the atomist bot

---
+++ src/main/java/com/lazorjack/FantasyFootballApplication.java
@@ -0,0 +1,12 @@
+package com.lazorjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyFootballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyFootballApplication.class, args);
+    }
+}
